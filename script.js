e2b12828f18c589fd58e09df7f6c9aa52f14b68e
estero-ar <veintiunoarca@gmail.com>
2025-10-15 13:45:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -48,3 +48,13 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 });
+
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+if (hamburger) {
+  hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+  });
+}
+
